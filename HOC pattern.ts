07584dbb529f22cba22136241c579d3435df8c73
Sethuramalingam Duraisamy <59419619+Sethuramalingam-art@users.noreply.github.com>
2023-05-02 23:49:35 +0530

    Add files via upload

---
+++ HOC pattern.ts
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+const higherOrderComponent = (DecoratedComponent) => {
+  class HOC extends Component {
+    render() {
+      return <DecoratedComponent />;
+    }
+  }
+  return HOC;
+};
